1afbd48f30f0f00987624d50dd2fa637fcb73af5
Jonatha Leite Santos <jonatha.eti@gmail.com>
2023-05-29 14:37:31 -0300

    Teste 02

---
+++ Aula7.Heranca/Padrao.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula7.Heranca
+{
+    public class Padrao
+    {
+        public Padrao(int p)
+        {
+
+        }
+    }
+    public class Automovel : Padrao
+    {
+        public string modelo { get; set; }
+        public Automovel() : base(20) // construtor
+        {
+
+        }
+    }
+
+    interface ICarro
+    {
+        void Modelo();
+    }
+    interface IMotorizado
+    {
+        void Ligar();
+        void Desligar();
+    }
+
+    public class Moto : Padrao, IMotorizado, ICarro  // classe primeiro depois Interfaces.
+    {
+        public Moto() : base(20)
+        {
+
+        }
+        public void Ligar() { }
+        public void Desligar() { }
+        public void Modelo() { }
+
+    }
+    interface IExamploA { }
+    interface IExamploB { }
+    interface IExamploC : IExamploA, IExamploB // implementação multiplua 
+    { }
+
+    public abstract class Animal
+    {
+        public abstract void MostrarPatas(); 
+
+        public int quantidadePatas { get; set; }
+        public void Falar() { }
+        public void EmitirSom(Animal a)
+        {
+            a.Falar();
+        }
+    }
+    public class Gato : Animal
+    {
+        public override void MostrarPatas()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+}
---
+++ Aula7.Heranca/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Aula7.Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            var moto = new Moto();
+
+            var a = new Gato();
+            a.EmitirSom(a);
+            a.MostrarPatas();
+
+            Console.ReadKey();
+        }
+    }
+}
