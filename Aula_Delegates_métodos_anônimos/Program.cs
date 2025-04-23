7031beb45c5400fd98fc04212a5c4b731f4d0b6e
Jonatha Leite Santos <jonatha.eti@gmail.com>
2025-04-23 19:16:31 -0300

    Delegates genéricos: Func, Action e Predicate

--- Aula_Delegates_métodos_anônimos/Program.cs
+++ Aula_Delegates_métodos_anônimos/Program.cs
@@ -7,7 +7,15 @@
 
         public delegate void MetodoDelegateLambda(int idade);
         public delegate int MetodoDelegateLambda2(string idade);
-        public static void utilizaDelegate(MetodoDelegate metodoDelegate)
+
+
+        //public delegate int MetodoDelegateGenerico(string idade);
+
+        //Func<> Tem até 16 parametro de entrada e um parametro de retorno
+        //Action<> Tem apenas parametros de entrada;
+        // Predicate<> Tem um tipo de entrada e retona TRUE ou FALSO
+
+        public static void utilizaDelegate(Func<string,int> metodoDelegate) // Delegate Generico até 17 parametros
         {
             var letras = metodoDelegate(" Maria");
             Console.WriteLine($"Quantidade de LEtras: {letras}");
@@ -16,11 +24,26 @@
         static void Main(string[] args)
         {
 
+            Predicate<int> pred = (num) => true; // delegate generico 
+            Predicate<int> pred1 = (num) => num%2==0;
+
+            Console.WriteLine("Resultado do Predicado: " + pred(1) + " - " + pred1(3));
+
+
+
             utilizaDelegate(delegate (string n)
             {
                 Console.WriteLine(" Nome: " + n);
                 return n.Length;
             });
+
+            Func<string, int> metodoDelegateGenerico = (n) =>
+            {
+                Console.WriteLine(" Nome " + n);
+                return n.Length; 
+            };
+
+            metodoDelegateGenerico(" Joantha ");
 
             MetodoDelegateLambda metodoLambda = (i) => { Console.WriteLine(" O bnúmero é " + i); };
 
